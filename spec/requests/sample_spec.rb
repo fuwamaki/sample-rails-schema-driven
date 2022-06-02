cd4a3a168743bca75bc2a7dc537616fef19f21d4
fuwamaki <variety.fuwa.3@gmail.com>
2022-06-02 17:19:19 +0900

    realize rspec cofirmation about openapi

--- spec/requests/sample_spec.rb
+++ spec/requests/sample_spec.rb
@@ -4,7 +4,7 @@ RSpec.describe "Samples", type: :request do
   describe 'GET /users' do
     it do
       get url_for(%i[users])
-      assert_response_schema_confirm(200)
+      assert_response_schema_confirm
     end
   end
 end
