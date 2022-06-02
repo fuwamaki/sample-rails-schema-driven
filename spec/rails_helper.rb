5ea3d17c8dcf37b034c5b61a6129716e60063b7a
fuwamaki <variety.fuwa.3@gmail.com>
2022-06-02 13:10:16 +0900

    setup committee-rails on spec/rails_helper.rb

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -43,4 +43,12 @@ RSpec.configure do |config|
   config.filter_rails_from_backtrace!
   # arbitrary gems may also be filtered via:
   # config.filter_gems_from_backtrace("gem name")
+
+  config.include Committee::Rails::Test::Methods
+  config.add_setting :committee_options
+  config.committee_options = {
+    schema_path: Rails.root.join('etc', 'openapi.yaml').to_s,
+    query_hash_key: 'rack.request.query_hash',
+    parse_response_by_content_type: false
+  }
 end
